b606551a9714f77cb32ddfd9e87b33213475f2d8
AnandXtechon <anand.pande@xtechon.com>
2025-06-21 12:15:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:8081", // ✅ Update if your frontend is hosted elsewhere
+    origin: "https://service-erp-ui.onrender.com", // ✅ Update if your frontend is hosted elsewhere
     credentials: true,
 };
 app.use(cors(corsOptions));
